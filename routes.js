6ea2a309a9d1bbb2ac41f8cf01f8f343f916cc4a
hankaibo <hankaibo@hotmail.com>
2016-08-09 14:05:35 +0800

    fix

--- routes.js
+++ routes.js
@@ -7,6 +7,11 @@ configRoutes = function (app, server) {
   });
   app.all('/:obj_type/*?', function (request, response, next) {
     response.contentType('json');
+    response.header("Access-Control-Allow-Origin","*");
+    response.header("Access-Control-Allow-Headers","X-Requested-With");
+    response.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    response.header("X-Powered-By","3.2.1");
+    response.header("Content-Type","application/json;charset=utf-8");
     next();
   });
   app.get('/:obj_type/list', function (request, response) {
@@ -29,6 +34,18 @@ configRoutes = function (app, server) {
     response.send({
       title: request.params.obj_type + ' with id ' + request.params.id + ' deleded'
     });
+  });
+
+  //about
+  app.get('/dashboard/aboutList', function (request, response) {
+    var data={};
+    var ABOUT_LIST = [
+      { id: 11, name: '胡瑞香', url: '/img/angular.png' },
+      { id: 12, name: '吕颖萍', url: '/img/angular.png' },
+      { id: 13, name: '徐霞', url: '/img/angular.png' }
+    ];
+    data['data']=ABOUT_LIST;
+    response.send(data);
   });
 };
 module.exports = { configRoutes: configRoutes }
